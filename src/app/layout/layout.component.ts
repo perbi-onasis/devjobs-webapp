feab49465d0a4f90eace898968b54908fe42e982
perbi.onasis <onasisyawperbi@gmail.com>
2024-09-02 15:25:46 +0000

    replace the bg image with an image element

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {SearchBarComponent} from '../components/search-bar/search-bar.component'
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
